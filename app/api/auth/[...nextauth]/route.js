0ec69e3dae4949aea4c200024b506d4e5b3170ad
Abraham <abrahamycaldera@gmail.com>
2024-07-09 17:17:21 -0400

    add configuration to next-auth

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import prisma from "../../../../libs/prisma/prisma";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const userFound = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!userFound) throw new Error("No user found");
+
+        const matchPassword = await bcrypt.compare(
+          credentials.password,
+          userFound.password
+        );
+
+        if (!matchPassword) throw new Error("Wrong password");
+
+        return {
+          id: userFound.id,
+          name: userFound.username,
+          email: userFound.email,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ app/api/auth/signup/route.js
@@ -0,0 +1,55 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import prisma from "@/libs/prisma/prisma";
+
+export async function POST(request) {
+  try {
+    const data = await request.json();
+
+    // email
+    const userFound = await prisma.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (userFound)
+      return NextResponse.json({
+        message: "Email already exists",
+      });
+
+    // username
+    const usernameFound = await prisma.user.findUnique({
+      where: {
+        username: data.username,
+      },
+    });
+
+    if (usernameFound)
+      return NextResponse.json({
+        message: "Username already exists",
+      });
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        username: data.username,
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...user } = newUser;
+
+    return NextResponse.json(user);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
