206b368a455550550e9a9c0c9b0161735b68c134
Abraham <abrahamycaldera@gmail.com>
2024-07-10 03:26:49 -0400

    add room styles page

--- components/landing-page/Nav/index.tsx
+++ components/landing-page/Nav/index.tsx
@@ -87,21 +87,23 @@ export default function NavMenu() {
             <DropdownMenu aria-label="Profile Actions" variant="flat">
               <DropdownItem key="profile" className="h-14 gap-2">
                 <p className="font-semibold">Signed in as</p>
-                <p className="font-semibold">zoey@example.com</p>
+                <p className="font-semibold">{session.user.email}</p>
               </DropdownItem>
-              <DropdownItem key="settings">My Settings</DropdownItem>
+              <DropdownItem key="settings">Perfil</DropdownItem>
               <DropdownItem key="team_settings">Team Settings</DropdownItem>
-              <DropdownItem key="analytics">Analytics</DropdownItem>
               <DropdownItem key="system">System</DropdownItem>
               <DropdownItem key="configurations">Configurations</DropdownItem>
-              <DropdownItem key="help_and_feedback">
-                Help & Feedback
-              </DropdownItem>
-              <DropdownItem key="logout" color="danger">
-                Log Out
-              </DropdownItem>
+                <DropdownItem key="logout" color="danger">
+              <NextLink href="/api/auth/signout">
+                  Cerrar sesión
+              </NextLink>
+                </DropdownItem>
             </DropdownMenu>
           </Dropdown>
+          
+          <NavbarItem>
+            <ThemeSwitcher />
+          </NavbarItem>
         </NavbarContent>
       ) : (
         <NavbarContent justify="center">
