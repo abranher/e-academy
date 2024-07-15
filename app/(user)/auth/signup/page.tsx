501ab644e34e57aaea12204634c7bf301de66e54
Abraham Hernandez <abrahamycaldera@gmail.com>
2024-07-15 02:58:33 -0400

    Add boxbase to signin and signup

--- app/(user)/auth/signup/page.tsx
+++ app/(user)/auth/signup/page.tsx
@@ -2,6 +2,7 @@
 
 import { EyeFilledIcon } from "@/components/icons/EyeFilledIcon";
 import { EyeSlashFilledIcon } from "@/components/icons/EyeSlashFilledIcon";
+import BoxBase from "@/components/landing-page/Boxes/BoxBase";
 import { Button, Input } from "@nextui-org/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -50,251 +51,190 @@ export default function SignupPage() {
 
   return (
     <>
-      <section className="relative z-10 overflow-hidden pb-16 pt-24 md:pb-20 lg:pb-28 flex justify-center items-center">
-        <div className="container px-4">
-          <div className="-mx-4 flex flex-wrap">
-            <div className="w-full px-4">
-              <div className="shadow-md mx-auto max-w-[500px] rounded bg-white px-6 py-10 dark:bg-dark sm:p-[60px]">
-                <h3 className="mb-3 text-center text-2xl font-bold text-black dark:text-white sm:text-3xl">
-                  Crea tu cuenta
-                </h3>
-                <p className="mb-11 text-center text-base font-medium text-body-color">
-                  Es totalmente gratis y súper fácil.
-                </p>
-                <form onSubmit={onSubmit} className="w-full">
-                  <div className="my-8">
-                    <Input
-                      size="lg"
-                      fullWidth
-                      label="Nombre de Usuario"
-                      variant="bordered"
-                      {...register("username", {
-                        required: {
-                          value: true,
-                          message: "El nombre de usuario es requerido!",
-                        },
-                      })}
-                      type="text"
-                      className="max-w-xs"
-                    />
-                    {errors.username && (
-                      <span className="text-red-500">
-                        {errors.username.message}
-                      </span>
-                    )}
-                  </div>
+      <BoxBase>
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4">
+            <div className="shadow-md mx-auto max-w-[500px] rounded bg-white px-6 py-10 dark:bg-dark sm:p-[60px]">
+              <h3 className="mb-3 text-center text-2xl font-bold text-black dark:text-white sm:text-3xl">
+                Crea tu cuenta
+              </h3>
+              <p className="mb-11 text-center text-base font-medium text-body-color">
+                Es totalmente gratis y súper fácil.
+              </p>
+              <form onSubmit={onSubmit} className="w-full">
+                <div className="my-8">
+                  <Input
+                    size="lg"
+                    fullWidth
+                    label="Nombre de Usuario"
+                    variant="bordered"
+                    {...register("username", {
+                      required: {
+                        value: true,
+                        message: "El nombre de usuario es requerido!",
+                      },
+                    })}
+                    type="text"
+                    className="max-w-xs"
+                  />
+                  {errors.username && (
+                    <span className="text-red-500">
+                      {errors.username.message}
+                    </span>
+                  )}
+                </div>
 
-                  <div className="my-8">
-                    <Input
-                      size="lg"
-                      fullWidth
-                      label="Correo"
-                      variant="bordered"
-                      {...register("email", {
-                        required: {
-                          value: true,
-                          message: "El correo es requerido!",
-                        },
-                      })}
-                      type="email"
-                      className="max-w-xs"
-                    />
-                    {errors.email && (
-                      <span className="text-red-500">
-                        {errors.email.message}
-                      </span>
-                    )}
-                  </div>
+                <div className="my-8">
+                  <Input
+                    size="lg"
+                    fullWidth
+                    label="Correo"
+                    variant="bordered"
+                    {...register("email", {
+                      required: {
+                        value: true,
+                        message: "El correo es requerido!",
+                      },
+                    })}
+                    type="email"
+                    className="max-w-xs"
+                  />
+                  {errors.email && (
+                    <span className="text-red-500">{errors.email.message}</span>
+                  )}
+                </div>
 
-                  <div className="my-8">
-                    <Input
-                      size="lg"
-                      fullWidth
-                      label="Contraseña"
-                      variant="bordered"
-                      {...register("password", {
-                        required: {
-                          value: true,
-                          message: "La contraseña es requerida!",
-                        },
-                      })}
-                      endContent={
-                        <button
-                          className="focus:outline-none"
-                          type="button"
-                          onClick={toggleVisibility}
-                        >
-                          {isVisible ? (
-                            <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                          ) : (
-                            <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                          )}
-                        </button>
-                      }
-                      type={isVisible ? "text" : "password"}
-                      className="max-w-xs"
-                    />
-                    {errors.password && (
-                      <span className="text-red-500">
-                        {errors.password.message}
-                      </span>
-                    )}
-                  </div>
+                <div className="my-8">
+                  <Input
+                    size="lg"
+                    fullWidth
+                    label="Contraseña"
+                    variant="bordered"
+                    {...register("password", {
+                      required: {
+                        value: true,
+                        message: "La contraseña es requerida!",
+                      },
+                    })}
+                    endContent={
+                      <button
+                        className="focus:outline-none"
+                        type="button"
+                        onClick={toggleVisibility}
+                      >
+                        {isVisible ? (
+                          <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                        ) : (
+                          <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                        )}
+                      </button>
+                    }
+                    type={isVisible ? "text" : "password"}
+                    className="max-w-xs"
+                  />
+                  {errors.password && (
+                    <span className="text-red-500">
+                      {errors.password.message}
+                    </span>
+                  )}
+                </div>
 
-                  <div className="my-8">
-                    <Input
-                      size="lg"
-                      fullWidth
-                      label="Confirma contraseña"
-                      variant="bordered"
-                      {...register("confirmPassword", {
-                        required: {
-                          value: true,
-                          message: "La confirmación es requerida!",
-                        },
-                      })}
-                      endContent={
-                        <button
-                          className="focus:outline-none"
-                          type="button"
-                          onClick={toggleVisibility}
-                        >
-                          {isVisible ? (
-                            <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                          ) : (
-                            <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                          )}
-                        </button>
-                      }
-                      type={isVisible ? "text" : "password"}
-                      className="max-w-xs"
-                    />
-                    {errors.confirmPassword && (
-                      <span className="text-red-500">
-                        {errors.confirmPassword.message}
-                      </span>
-                    )}
-                  </div>
+                <div className="my-8">
+                  <Input
+                    size="lg"
+                    fullWidth
+                    label="Confirma contraseña"
+                    variant="bordered"
+                    {...register("confirmPassword", {
+                      required: {
+                        value: true,
+                        message: "La confirmación es requerida!",
+                      },
+                    })}
+                    endContent={
+                      <button
+                        className="focus:outline-none"
+                        type="button"
+                        onClick={toggleVisibility}
+                      >
+                        {isVisible ? (
+                          <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                        ) : (
+                          <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                        )}
+                      </button>
+                    }
+                    type={isVisible ? "text" : "password"}
+                    className="max-w-xs"
+                  />
+                  {errors.confirmPassword && (
+                    <span className="text-red-500">
+                      {errors.confirmPassword.message}
+                    </span>
+                  )}
+                </div>
 
-                  <div className="mb-8 flex">
-                    <label
-                      htmlFor="checkboxLabel"
-                      className="flex cursor-pointer select-none text-sm font-medium text-body-color"
-                    >
-                      <div className="relative">
-                        <input
-                          type="checkbox"
-                          id="checkboxLabel"
-                          className="sr-only"
-                        />
-                        <div className="box mr-4 mt-1 flex h-5 w-5 items-center justify-center rounded border border-body-color border-opacity-20 dark:border-white dark:border-opacity-10">
-                          <span className="opacity-0">
-                            <svg
-                              width="11"
-                              height="8"
-                              viewBox="0 0 11 8"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                d="M10.0915 0.951972L10.0867 0.946075L10.0813 0.940568C9.90076 0.753564 9.61034 0.753146 9.42927 0.939309L4.16201 6.22962L1.58507 3.63469C1.40401 3.44841 1.11351 3.44879 0.932892 3.63584C0.755703 3.81933 0.755703 4.10875 0.932892 4.29224L0.932878 4.29225L0.934851 4.29424L3.58046 6.95832C3.73676 7.11955 3.94983 7.2 4.1473 7.2C4.36196 7.2 4.55963 7.11773 4.71406 6.9584L10.0468 1.60234C10.2436 1.4199 10.2421 1.1339 10.0915 0.951972ZM4.2327 6.30081L4.2317 6.2998C4.23206 6.30015 4.23237 6.30049 4.23269 6.30082L4.2327 6.30081Z"
-                                fill="#3056D3"
-                                stroke="#3056D3"
-                                strokeWidth="0.4"
-                              />
-                            </svg>
-                          </span>
-                        </div>
-                      </div>
-                      <span>
-                        Al crear una cuenta significa que acepta los{" "}
-                        <a href="#0" className="text-primary hover:underline">
-                          Términos y condiciones
-                        </a>
-                        , y nuestra{" "}
-                        <a href="#0" className="text-primary hover:underline">
-                          Política de privacidad.
-                        </a>
-                      </span>
-                    </label>
-                  </div>
-                  <div className="mb-6">
-                    <Button color="primary" size="lg" type="submit">
-                      Crear cuenta
-                    </Button>
-                  </div>
-                </form>
-                <p className="text-center text-base font-medium text-body-color">
-                  Ya tienes una cuenta?{" "}
-                  <Link
-                    href="/auth/signin"
-                    className="text-primary hover:underline"
+                <div className="mb-8 flex">
+                  <label
+                    htmlFor="checkboxLabel"
+                    className="flex cursor-pointer select-none text-sm font-medium text-body-color"
                   >
-                    Iniciar sesión
-                  </Link>
-                </p>
-              </div>
+                    <div className="relative">
+                      <input
+                        type="checkbox"
+                        id="checkboxLabel"
+                        className="sr-only"
+                      />
+                      <div className="box mr-4 mt-1 flex h-5 w-5 items-center justify-center rounded border border-body-color border-opacity-20 dark:border-white dark:border-opacity-10">
+                        <span className="opacity-0">
+                          <svg
+                            width="11"
+                            height="8"
+                            viewBox="0 0 11 8"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M10.0915 0.951972L10.0867 0.946075L10.0813 0.940568C9.90076 0.753564 9.61034 0.753146 9.42927 0.939309L4.16201 6.22962L1.58507 3.63469C1.40401 3.44841 1.11351 3.44879 0.932892 3.63584C0.755703 3.81933 0.755703 4.10875 0.932892 4.29224L0.932878 4.29225L0.934851 4.29424L3.58046 6.95832C3.73676 7.11955 3.94983 7.2 4.1473 7.2C4.36196 7.2 4.55963 7.11773 4.71406 6.9584L10.0468 1.60234C10.2436 1.4199 10.2421 1.1339 10.0915 0.951972ZM4.2327 6.30081L4.2317 6.2998C4.23206 6.30015 4.23237 6.30049 4.23269 6.30082L4.2327 6.30081Z"
+                              fill="#3056D3"
+                              stroke="#3056D3"
+                              strokeWidth="0.4"
+                            />
+                          </svg>
+                        </span>
+                      </div>
+                    </div>
+                    <span>
+                      Al crear una cuenta significa que acepta los{" "}
+                      <a href="#0" className="text-primary hover:underline">
+                        Términos y condiciones
+                      </a>
+                      , y nuestra{" "}
+                      <a href="#0" className="text-primary hover:underline">
+                        Política de privacidad.
+                      </a>
+                    </span>
+                  </label>
+                </div>
+                <div className="mb-6">
+                  <Button color="primary" size="lg" type="submit">
+                    Crear cuenta
+                  </Button>
+                </div>
+              </form>
+              <p className="text-center text-base font-medium text-body-color">
+                Ya tienes una cuenta?{" "}
+                <Link
+                  href="/auth/signin"
+                  className="text-primary hover:underline"
+                >
+                  Iniciar sesión
+                </Link>
+              </p>
             </div>
           </div>
         </div>
-        <div className="absolute left-0 top-0 z-[-1]">
-          <svg
-            width="1440"
-            height="969"
-            viewBox="0 0 1440 969"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <mask
-              id="mask0_95:1005"
-              style={{ maskType: "alpha" }}
-              maskUnits="userSpaceOnUse"
-              x="0"
-              y="0"
-              width="1440"
-              height="969"
-            >
-              <rect width="1440" height="969" fill="#090E34" />
-            </mask>
-            <g mask="url(#mask0_95:1005)">
-              <path
-                opacity="0.1"
-                d="M1086.96 297.978L632.959 554.978L935.625 535.926L1086.96 297.978Z"
-                fill="url(#paint0_linear_95:1005)"
-              />
-              <path
-                opacity="0.1"
-                d="M1324.5 755.5L1450 687V886.5L1324.5 967.5L-10 288L1324.5 755.5Z"
-                fill="url(#paint1_linear_95:1005)"
-              />
-            </g>
-            <defs>
-              <linearGradient
-                id="paint0_linear_95:1005"
-                x1="1178.4"
-                y1="151.853"
-                x2="780.959"
-                y2="453.581"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#4A6CF7" />
-                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
-              </linearGradient>
-              <linearGradient
-                id="paint1_linear_95:1005"
-                x1="160.5"
-                y1="220"
-                x2="1099.45"
-                y2="1192.04"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#4A6CF7" />
-                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
-              </linearGradient>
-            </defs>
-          </svg>
-        </div>
-      </section>
+      </BoxBase>
     </>
   );
 }
--- components/landing-page/Boxes/BoxBase.tsx
+++ components/landing-page/Boxes/BoxBase.tsx
@@ -4,7 +4,7 @@ export default function BoxBase({
   children: React.ReactNode;
 }>) {
   return (
-    <section className="overflow-hidden bg-white pb-16 pt-[120px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px] flex justify-center items-center">
+    <section className="overflow-hidden bg-white py-16 dark:bg-gray-dark md:py-[40px] xl:py-[70px] 2xl:py-[100px] flex justify-center items-center">
       <section className="px-6 py-20 w-full max-w-[1536px]">{children}</section>
     </section>
   );
